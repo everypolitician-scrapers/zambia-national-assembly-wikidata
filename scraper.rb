d264aba215f32bf13dc5d48d9a490e92e65c859c
Tony Bowden <tony@tmtm.com>
2018-03-07 11:56:50 +0000

    Also fetch via Wikidata P39s

--- scraper.rb
+++ scraper.rb
@@ -4,4 +4,8 @@
 require 'wikidata/fetcher'
 
 names = WikiData::Category.new( 'Category:Members of the National Assembly (Zambia)', 'en').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
+
+query = 'SELECT DISTINCT ?item WHERE { ?item p:P39/pq:P2937 wd:Q45380990 }'
+p39s = EveryPolitician::Wikidata.sparql(query)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s, names: { en: names })
